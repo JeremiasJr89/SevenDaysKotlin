3dc37f401d338579bf22effe4fc59eeacd74741f
JeremiasJr89 <108739405+JeremiasJr89@users.noreply.github.com>
2023-08-07 17:41:21 -0300

    Day 4

---
+++ app/src/main/java/com/example/sevendayskotlin/model/Movie.kt
@@ -0,0 +1,8 @@
+package com.example.sevendayskotlin.model
+
+data class Movie (
+    val title: String,
+    val image: String,
+    val rating: Double,
+    val year: Int
+)
--- app/src/main/java/com/example/sevendayskotlin/ui/theme/screnn/movie.kt
+++ app/src/main/java/com/example/sevendayskotlin/ui/theme/screnn/movie.kt
@@ -1,8 +1,5 @@
-package com.example.sevendayskotlin
+package com.example.sevendayskotlin.ui.theme.screnn
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,6 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,46 +27,41 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.example.sevendayskotlin.model.Movie
-import com.example.sevendayskotlin.ui.theme.SevenDaysKotlinTheme
-import com.example.sevendayskotlin.ui.theme.screnn.MovieItem
+import com.example.sevendayskotlin.model.Movie as Movie1
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            SevenDaysKotlinTheme {
-                Surface {
-                    Box(modifier = Modifier.fillMaxSize()) {
 
-                        val movies = listOf(
-                            Movie(
-                                title = "The Shawshank Redemption",
-                                image = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
-                                rating = 9.2,
-                                year = 1994
-                            ),
-                            Movie(
-                                title = "The Godfather",
-                                image = "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
-                                rating = 9.2,
-                                year = 1972
-                            ),
-                            Movie(
-                                title = "The Dark Knight",
-                                image = "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX128_CR0,3,128,176_AL_.jpg",
-                                rating = 9.0,
-                                year = 2008
-                            )
-                        )
-                        LazyColumn {
-                            items(movies) { movie -> MovieItem(movie) }
-                        }
-                    }
+@Composable
+fun Movie(movies: List<Movie1>) {
+    MaterialTheme(
+    ) {
+        Surface {
+            Box(modifier = Modifier.fillMaxSize()) {
+
+                val movies = listOf(
+                    Movie1(
+                        title = "The Shawshank Redemption",
+                        image = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                        rating = 9.2,
+                        year = 1994
+                    ),
+                    Movie1(
+                        title = "The Godfather",
+                        image = "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
+                        rating = 9.2,
+                        year = 1972
+                    ),
+                    Movie1(
+                        title = "The Dark Knight",
+                        image = "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                        rating = 9.0,
+                        year = 2008
+                    )
+                )
+                LazyColumn {
+                    items(movies) { movie -> MovieItem(movie) }
                 }
             }
         }
@@ -76,9 +69,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyComposable(movies: Any?) {
-    val image =
-        "https://www.imdb.com/title/tt6791350/mediaviewer/rm1222125825/?ref_=tt_ov_i"
+fun MovieItem(movie: Movie1) {
+    val image = "https://www.imdb.com/title/tt6791350/mediaviewer/rm1222125825/?ref_=tt_ov_i"
     Column(
         Modifier
             .padding(8.dp)
@@ -150,11 +142,3 @@ fun MyComposable(movies: Any?) {
     }
 }
 
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SevenDaysKotlinTheme {
-        MyComposable(movies = null)
-    }
-}
